859c7d121a4b9c6e3c58c8c5e0988d0ca2e28a1e
James Haug <james.haug@outlook.com>
2019-11-13 14:45:59 -0500

    Set up basic exception handling for startup purposes.

--- Thunder.Blog/Startup.cs
+++ Thunder.Blog/Startup.cs
@@ -11,15 +11,31 @@ namespace Thunder.Blog
 {
     public class Startup
     {
+        private static Exception _exception;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            try
+            {
+                throw new InvalidOperationException("This doesn't work!");
+            }
+            catch(Exception ex)
+            {
+                _exception = ex;
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if(_exception != null)
+            {
+                RenderExceptionPage(app, _exception);
+                return;
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -28,6 +44,15 @@ namespace Thunder.Blog
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private static void RenderExceptionPage(IApplicationBuilder app, Exception exception)
+        {
+            app.UseDeveloperExceptionPage();
+            app.Run((context) =>
+            {
+                throw new Exception("Something went wrong while trying to start the application. See inner exception for details.", exception);
             });
         }
     }
